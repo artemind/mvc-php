402db3c7cb981df1e251faa09a53d5886e4d4f89
Artem Yeremenko <contact@artemind.dev>
2017-05-13 23:52:26 +0300

    добавил метод для получения страницы ошибки

--- core/Controller.php
+++ core/Controller.php
@@ -7,4 +7,23 @@ class Controller
     public function __construct() {
         $this->view = View::getInstance();
     }
+
+    public function getErrorPage($message, $title = "Ошибка!", $type = 'default') {
+    	$title = trim(strip_tags($title));
+        $message = trim(strip_tags($message));
+        $types = trim(strip_tags($type));
+        $types = [
+            'default',
+            'primary',
+            'success',
+            'info',
+            'danger',
+            'warning',
+        ];
+        if(!in_array($type, $types)) {
+            $type = 'default';
+        }
+    	$this->view->render('errors/index', compact('message', 'title', 'type'));
+    	return false;
+    }
 }
